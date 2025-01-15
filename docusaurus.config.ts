c15390d0f890155990be3045449dd7dcea9a5505
aYuan <w3a68vjp@gmail.com>
2025-01-15 16:05:33 +0800

    Revert "Modify url"
    This reverts commit cce494e0e40143c86605417c6ee0ac63a2466777.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,10 +11,8 @@ const config: Config = {
   themes: ['@docusaurus/theme-mermaid'],
   title: '資料字典網站',
   tagline: 'focus on your content',
-  url: 'https://cvmdoc.github.io/',
-  baseUrl: '/',
-  // url: 'https://hrisdb-uat.test',
-  // baseUrl: '/doc',
+  url: 'https://hrisdb-uat.test',
+  baseUrl: '/doc',
   projectName: 'cvmdoc.github.io', // Usually your repo name.
   organizationName: 'cvmdoc', // Usually your GitHub org/user name.
   onBrokenLinks: 'throw',
